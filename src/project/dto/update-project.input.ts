564d1e763741af289e77db416352752c16130c13
boby177 <bomasu37@gmail.com>
2022-12-29 14:16:20 +0700

    create mutation for project

--- src/project/dto/update-project.input.ts
+++ src/project/dto/update-project.input.ts
@@ -2,7 +2,13 @@ import { CreateProjectInput } from './create-project.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateProjectInput extends PartialType(CreateProjectInput) {
+export class UpdateProjectInput {
+  @Field()
+  id: string;
+
+  @Field()
+  name: string;
+
   @Field(() => Int)
-  id: number;
+  code: number;
 }
