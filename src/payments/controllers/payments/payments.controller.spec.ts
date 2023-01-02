f232ea053312db789ae4110dedcc347ab48ade0e
boby177 <bomasu37@gmail.com>
2023-01-02 16:41:46 +0700

    added unit testing from services

--- src/payments/controllers/payments/payments.controller.spec.ts
+++ src/payments/controllers/payments/payments.controller.spec.ts
@@ -1,9 +1,12 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Request, Response } from 'express';
+import { PaymentsService } from '../../services/payments/payments.service';
 import { PaymentsController } from './payments.controller';
 
 describe('PaymentsController', () => {
   let controller: PaymentsController;
+  let paymentsService: PaymentsService;
 
   const requestMock = {
     query: {},
@@ -21,17 +24,30 @@ describe('PaymentsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaymentsController],
+      providers: [
+        {
+          provide: 'PAYMENTS_SERVICE',
+          useValue: {
+            createPayment: jest.fn((x) => x),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<PaymentsController>(PaymentsController);
+    paymentsService = module.get<PaymentsService>('PAYMENTS_SERVICE');
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
+  it('paymentsService should be defined', () => {
+    expect(paymentsService).toBeDefined();
+  });
+
   describe('getPayments', () => {
-    it('should return a status 400', () => {
+    it('should return a status 400', async () => {
       controller.getPayments(requestMock, responseMock);
       expect(responseMock.status).toHaveBeenCalledWith(400);
       expect(statusResponseMock.send).toHaveBeenCalledWith({
@@ -39,12 +55,30 @@ describe('PaymentsController', () => {
       });
     });
   });
-  it('should return a status of 200 when query [arams are present]', () => {
+  it('should return a status of 200 when query params are present', async () => {
     requestMock.query = {
       count: '10',
       page: '1',
     };
-    controller.getPayments(requestMock, responseMock);
+    await controller.getPayments(requestMock, responseMock);
     expect(responseMock.send).toHaveBeenCalledWith(200);
+  });
+
+  describe('createPayment', () => {
+    it('should throw an error', async () => {
+      jest
+        .spyOn(paymentsService, 'createPayment')
+        .mockImplementationOnce(() => {
+          throw new BadRequestException();
+        });
+      try {
+        const response = await controller.createPayment({
+          email: 'bobys@gmail.com',
+          price: 100,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
   });
 });
