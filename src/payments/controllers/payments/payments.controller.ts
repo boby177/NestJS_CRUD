873d43cd4a71d21d5113d5d0eb94c6ff21f93345
boby177 <bomasu37@gmail.com>
2023-01-02 16:43:26 +0700

    updated using payment service

--- src/payments/controllers/payments/payments.controller.ts
+++ src/payments/controllers/payments/payments.controller.ts
@@ -1,8 +1,15 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Body, Inject, Post } from '@nestjs/common/decorators';
 import { Request, Response } from 'express';
+import { CreatePaymentDTO } from '../../dto/CreatePayment.dto';
+import { PaymentsService } from '../../services/payments/payments.service';
 
 @Controller('payments')
 export class PaymentsController {
+  constructor(
+    @Inject('PAYMENTS_SERVICE')
+    private readonly paymentsService: PaymentsService,
+  ) {}
   @Get()
   getPayments(@Req() request: Request, @Res() response: Response) {
     const { count, page } = request.query;
@@ -13,5 +20,11 @@ export class PaymentsController {
     } else {
       response.send(200);
     }
+  }
+
+  @Post('create')
+  async createPayment(@Body() createPaymentDTO: CreatePaymentDTO) {
+    const response = await this.paymentsService.createPayment(createPaymentDTO);
+    return response;
   }
 }
